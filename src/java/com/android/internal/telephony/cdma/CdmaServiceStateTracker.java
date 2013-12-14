526dfb95a84ea0a916a97961a0753da00ea6c174
Chris Holmes <chris@chris123nt.com>
2013-12-14 19:14:55 +0000

    Revert "Fix cdma data calls based on NV"
    Causing a wild issue on d2vzw and jfltevzw.
    If you connect to wifi, data will be broken when you disconnect
    unless you reboot the phone.  Reverting this and the dependant
    base change fixes the issue with data coming off wifi.
    
    This reverts commit 7cd6a99e125fe292eb6a78f8d8a307762eea320d.
    
    PS2: Retain some NV pieces
    PS3: Fix compile
    PS4: derp
    
    Change-Id: Iaad556f46bf5d35483f1c44c054945511d8c4234

--- src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
+++ src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -1114,11 +1114,10 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
             mPhone.notifyServiceStateChanged(mSS);
         }
 
-        // First notify detached, then rat changed, then attached - that's the way it
-        // happens in the modem.
-        // Behavior of recipients (DcTracker, for instance) depends on this sequence
-        // since DcTracker reloads profiles on "rat_changed" notification and sets up
-        // data call on "attached" notification.
+        if (hasCdmaDataConnectionAttached) {
+            mAttachedRegistrants.notifyRegistrants();
+        }
+
         if (hasCdmaDataConnectionDetached) {
             mDetachedRegistrants.notifyRegistrants();
         }
@@ -1126,10 +1125,6 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
         if (hasCdmaDataConnectionChanged || hasRilDataRadioTechnologyChanged) {
             notifyDataRegStateRilRadioTechnologyChanged();
             mPhone.notifyDataConnection(null);
-        }
-
-        if (hasCdmaDataConnectionAttached) {
-            mAttachedRegistrants.notifyRegistrants();
         }
 
         if (hasRoamingOn) {
